f1b0f4ed2cef9c259c5875ee7200dc718833ca97
Борзунов Андрей <andry73@yandex.ru>
2026-09-08 11:16:01 +0500

    test ci 26

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["158.160.178.202", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["158.160.186.173", "localhost", "127.0.0.1"]
 
 
 # Application definition
